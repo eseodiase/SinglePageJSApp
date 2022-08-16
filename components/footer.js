26df60bc94b0a85d73dcd60e8892a78e4ef7dcc9
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-08-16 16:50:44 +0100

    footer.js

---
+++ components/footer.js
@@ -0,0 +1,16 @@
+const footer = () =>{
+    return `<section class="allFooter">
+           <div class="footer_head">
+            <i class="fa fa-twitter"></i>
+            <i class="fa fa-instagram"></i>
+            <i class="fa fa-youtube"></i>
+            <i class="fa fa-whatsapp"></i>
+            <i class="fa fa-github"></i>
+        </div>
+        
+        <div class="footer_bottom">
+    <p> Developed by <a href="">Ese Odiase</a></p>
+        </div>
+</section>`;
+};
+export default footer;
