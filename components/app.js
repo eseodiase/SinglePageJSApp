acccde2b6d6f4fffdb7f9182c3ff89acaa0f56ac
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-08-16 16:55:21 +0100

    app.js

--- components/app.js
+++ components/app.js
@@ -1,12 +1,18 @@
 import header from "./header.js";
 import home from "./home.js";
+import about from "./about.js";
+import gallary from "./gallary.js";
+import connect from "./contact.js";
+import gallary from "./gallary.js";
+import footer from "./footer.js";
 
 function jsapp(){
 
 "use strict";
 
 document.querySelector(".header").innerHTML = ( header() );
-document.querySelector(".main").innerHTML = ( home() );
+document.querySelector(".main").innerHTML = ( home() + about() + gallery() + connect() );
+document.querySelecto(".footer"). innerHTML = ( footer () );
 };
 
 export default jsapp;
