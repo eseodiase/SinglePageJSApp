1ea8c79c72bccfba9e200fd7a9f1224c441d4573
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-09-12 20:46:33 +0100

    about.js

--- components/about.js
+++ components/about.js
@@ -4,17 +4,22 @@ const about_Page_Compound_Values={
     //classNames Used:
        ofImg: "aboutBGimg",
        art: "article",
-     //texts:
-     
     };
 const abt = about_Page_Compound_Values;
     
     return `  <section class="aboutPage">
-          <div class=${abt.ofImg}>
-           <img src="https://eseodiase.github.io/checking/3099563.jpg" alt="Image background for about page" height="212px" width="100%"/>
-          </div>
-          
           <div class=${abt.art}>
+          
+             <div class=${abt.ofImg}>
+             <figure>
+           <img src="https://eseodiase.github.io/SinglePageJSApp/images/about Us.jpg" alt="Image used for about us" height="212px" width="100%"/>
+           <figcaption>
+           <a href="https://www.freepik.com/vectors/javascript">Javascript vector created by vectorjuice.
+           </a>
+           </figcaption>
+             </figure>
+             </div>
+          
           <h3>About Us</h3>
               <article>
                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque posuere, ligula in finibus semper, nisi odio lobortis libero, eu aliquet diam massa vitae ipsum. Cras laoreet tortor a mi gravida, eu vestibulum metus venenatis. Aenean maximus lacus quis neque mollis accumsan. Fusce finibus aliquam purus id malesuada. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam lectus risus, pellentesque vitae nisi quis, rutrum blandit massa. Ut tincidunt ultrices molestie.
@@ -25,5 +30,5 @@ const abt = about_Page_Compound_Values;
               </article>
           </div>
       </section> `;
-}; 
+};
 export default about;
