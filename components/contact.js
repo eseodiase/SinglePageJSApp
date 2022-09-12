1b3311f03d6ce8b2627dcd2b756512392d22ce5e
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-09-12 20:50:07 +0100

    contact.js

--- components/contact.js
+++ components/contact.js
@@ -1,20 +1,33 @@
 const connect = () =>{
     return ` <section class="connectPage">
         <div class="container">
-            <div class="connectPart1">
+          
               <form>
               <h3>Connect with Us</h3>
-              <label>Name</label>
+              <label> 
+              <i class="fa fa-user"></i>
+              </label>
                   <input type="text" name="Name" placeholder="Your name"/><br />
+                  
                   <hr>
-                    <label>E-mail</label>
+                    <label> 
+                    <i class="fa fa-at"></i>
+                    </label>
                   <input type="email" name="E-mail" placeholder="Your e-mail"/>
                   <br />
+                  
                   <hr>
-                  <textarea name="Message" placeholder="Your message" rows="4" cols="30"></textarea><br /><br/>
+                <label class="txtA">
+                <i class="fa fa-envelope"></i>
+                </label>
+                  <textarea name="Message" placeholder="Your message" rows="6" cols="30"></textarea><br /><br/>
+                  
                   <hr>
-                  <button type="submit" class="submit">Send</button> 
+                  <button type="submit" class="submit">
+                  Send
+                  </button> 
               </form>
+                          
             </div> 
         </section>`;
 };
