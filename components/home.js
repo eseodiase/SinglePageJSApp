212ae4ff443c21c24f71f9ac5372b698122b360b
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-09-12 20:44:47 +0100

    home.js

--- components/home.js
+++ components/home.js
@@ -1,8 +1,8 @@
 const home =()=>{
 "use strict";
-  const  everyTextsUsedInsideHomePage = {
-        heading: "Single Page JS App",
-        article: "Quisque posuere, ligula in finibus semper, <br/>nisi odio lobortis libero. Rutrum blandit <br/>massa. Ut tincidunt ultrices molestie.",
+ const  everyTextsUsedInsideHomePage = {
+heading: "Single Page JS App",
+article: "Quisque posuere, ligula in finibus semper, <br/>nisi odio lobortis libero. Rutrum blandit <br/>massa. Ut tincidunt ultrices molestie.",
         
         subHeading1: "JavaScript Block",
         short1: "",
@@ -13,7 +13,7 @@ const home =()=>{
         
         //classNames used:
         container: "homePage",
-        wrap: "title_div",
+        wrap: "hero_div",
         div1: "part1",
         within: "within_part1",
         div2: "part2", // this div has been removed.
@@ -25,78 +25,95 @@ const home =()=>{
         div6: "box3",
     };
 const texts = everyTextsUsedInsideHomePage;
+
     return `<section class="homeP">
-    <div class=${texts.container}>
+<div class=${texts.container}>
     
-    <div class=${texts.wrap}>
-    
-  <div class=${texts.div1}>
-  <div class=${texts.within}>
-  <h1 class="desktopHeading">Single Page
-  <br/> JS App
-  </h1>            
-   <h1 class="mobileHeading">${texts.heading}</h1>
-   <p class="heroTxt">${texts.article}</p>
+<!--beginning of hero_div(part1 & part3 wraps) -->
+<div class=${texts.wrap}>
+
+<!--part 1 beginning -->      
+      <div class=${texts.div1}>
+      <div class=${texts.within}>
+  
+<h1 class="desktopHeading">
+  Single Page <br/> JS App
+</h1>  
+  
+<h1 class="mobileHeading">${texts.heading}</h1>
+<p class="heroTxt">${texts.article}</p>
+   
 <div class="forAction">
 <span class="action">Learn More</span>
 </div>
-</div>
-</div>
-  
-              
-               <div class=${texts.div3}>
+
+      </div>
+      </div>
+<!--part 1 ending -->      
+
+<!--part 2 has been removed -->      
+
+<!--part 3 beginning -->              
+<div class=${texts.div3}>
                
 <div class=${texts.wrap2}>
-               <h3 class="js">{}</h3>
-                   <h3>${texts.subHeading1}</h3>
-                   <p>
-  var name = "JS App";<br/>{<br/> console.log(name)<br/>};
-                   </p>
+<h3 class="js">{}</h3>
+<h3>${texts.subHeading1}</h3>
+<p>
+  var name = "JS App"; <br/>{<br/> console.log(name)<br/>};
+</p>
 </div>
                 
 <div class="txt_wrap">  
-                <h3 class="js">[]</h3> 
-                   <h3>${texts.subHeading2}</h3>
-                   <p>let arr = []; <br/>
+<h3 class="js">[]</h3> 
+<h3>${texts.subHeading2}</h3>
+<p>
+let arr = []; <br/>
 arr[0] = "ECMAScript"; <br/>
 arr[1] = "JavaScript"; <br/>
-console.log(arr[1]);</p>
+console.log(arr[1]);
+</p>
 </div>
                 
 <div class="txt_wrap">  
-                <h3 class="js">()</h3> 
-                   <h3>${texts.subHeading3}</h3>
-                   <p>const name = "JavaScript";<br/>
+<h3 class="js">()</h3> 
+<h3>${texts.subHeading3}</h3>
+<p>
+const name = "JavaScript";<br/>
 const re_name = name.replace<br/>("JavaScript", "JS App");<br/>
 console.log(re_name);
 </p>
 </div>
-                
-                </div>
-          </div>
-    
-          
+
+</div>
+<!--part 3 ending -->    
+
+</div>
+<!-- ending of hero_div(part1 & part3 wraps) -->
+        
+<!--box 1 to 3 beginning -->      
           <div>
-          
               <div class=${texts.div4}>
                  <p>CREATIVE HASHTAGES:</p>
                  <p>#designbyese</p>
               </div>
               
-               <div class=${texts.div5}>
+              <div class=${texts.div5}>
                  <p>Aenean maximus lacus quis neque mollis accumsan.</p>
               </div>
              
-               <div class=${texts.div6}>
+              <div class=${texts.div6}>
                  <i class="fa fa-twitter"></i>
                  <i class="fa fa-instagram"></i>
                  <i class="fa fa-youtube"></i>
                  <i class="fa fa-whatsapp"></i>
                  <i class="fa fa-github"></i>
-              </div>
+             </div>
           </div>
           <br/>
-          
+<!--box 1 to 3 ending -->      
+
+<!--footer beginning-->      
         <div class="home_bottom">
         <div class="footer_head">
       <i class="fa fa-twitter"></i>
@@ -109,10 +126,10 @@ console.log(re_name);
         <div class="footer_bottom">
    <p> Developed by <a href="">Ese Odiase</a></p>
         </div>
-        </div>    
+        </div>   
+<!--footer ending-->      
          
 </div>
 </section> `;
 };
-
 export default home;
